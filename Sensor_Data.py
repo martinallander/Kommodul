28d31e6c8856b188f9cbb708b8d82c3cb56d091a
martinallander <martin.allander@gmail.com>
2018-04-23 13:51:57 +0200

    Fixes to file strucure, new sensor data class

---
+++ Sensor_Data.py
@@ -0,0 +1,18 @@
+class Sensor_Data:
+    def __init__(self, acceleromter, gyrometer, ir, tof):
+        self.acceleromter = acceleromter
+        self.gyrometer = gyrometer
+        self.ir = ir
+        self.tof = tof
+    def __str__(self):
+        return """Gyrometer values: " + str(self.gyrometer) + 
+        "\n Acceleromter values: " + str(self.acceleromter) +
+        "\n IR values: " + ir_str() +
+        "\n ToF distance: " str(self.tof) """
+    def ir_str(self):
+    	formatted_ir = ""
+    	for i in range(len(self.ir)):
+    		formatted_ir += self.ir[i]
+    		if (i % 4) == 0:
+    			formatted_ir += "\n"
+    	return formatted_ir
