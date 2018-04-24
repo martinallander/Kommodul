6f35d85f94ab30f386d4ebbbc019815472d1a0f2
martinallander <martin.allander@gmail.com>
2018-04-24 10:50:06 +0200

    fixes to sensor_data

--- Sensor_Data.py
+++ Sensor_Data.py
@@ -5,11 +5,11 @@ class Sensor_Data:
         self.ir = ir
         self.tof = tof
     def __str__(self):
-        return "Gyrometer values: " + str(self.gyrometer) + "\n Acceleromter values: " + str(self.acceleromter) +   "\n IR values: " + ir_str() +"\n ToF distance: " + str(self.tof) 
+        return "Gyrometer values: " + str(self.gyrometer) + "\n Acceleromter values: " + str(self.acceleromter) +   "\n IR values: " + self.ir_str() +"\n ToF distance: " + str(self.tof) 
     def ir_str(self):
     	formatted_ir = ""
     	for i in range(len(self.ir)):
-    		formatted_ir += self.ir[i]
+    		formatted_ir += str(self.ir[i]) + " "
     		if (i % 4) == 0:
     			formatted_ir += "\n"
     	return formatted_ir
