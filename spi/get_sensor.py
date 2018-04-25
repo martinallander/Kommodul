85339e629d87e7d78431d3bfc6da6b671921deed
TheoW <thewe260@student.liu.se>
2018-04-25 09:55:21 +0000

    xD

--- spi/get_sensor.py
+++ spi/get_sensor.py
@@ -4,41 +4,82 @@ import struct
 import RPi.GPIO as GPIO
 #import keyboard
 
-sensmodul_spi = spidev.SpiDev()
-sensmodul_spi.open(0, 0)
-sensmodul_spi.max_speed_hz = 2000000
+def init_sens_modul():
+        sensmodul_spi = spidev.SpiDev()
+        sensmodul_spi.open(0, 0)
+        sensmodul_spi.max_speed_hz = 2000000
+        return sensmodul_spi
     
-
-styrmodul_spi = spidev.SpiDev()
-styrmodul_spi.open(0, 1)
-styrmodul_spi.max_speed_hz = 2000000
+def init_styr_modul():
+        styrmodul_spi = spidev.SpiDev()
+        styrmodul_spi.open(0, 1)
+        styrmodul_spi.max_speed_hz = 2000000
+        return styrmodul_spi
 
 print('Please wait.. \n .... \n ......... \n')
 
 #def format_sensor_data():
 
 def gpio_setup():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(4, GPIO.IN)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(4, GPIO.IN)
 
 def set_mode(module, mode_id):
-    module.mode = mode_id
-
-def write_sensmodul(input):
-    msb = input >> 8
-    lsb = input & 0xFF
-    sensmodul_spi.xfer([msb, lsb])
+        module.mode = mode_id
     
-def read_byte():
-	resp = sensmodul_spi.readbytes(1)
-	return resp[0]
+def read_byte(spi_buss):
+        resp = spi_buss.readbytes(1)
+        return resp[0]
 
 def unpack_bytes(bytes):
-	return struct.unpack("f", bytes)
+        return struct.unpack('f', bytes)
 
+def read_sensor_init(spi_buss):
+        start = read_byte(spi_buss)
+        if start == 0x00:
+                length = read_byte(spi_buss)
+                return length
+        return 0
+
+def request_ACC(spi_buss):
+        spi_buss.writebytes([0x01])
+
+def request_GYRO(spi_buss):
+        spi_buss.writebytes([0x02])
+
+def request_IR(spi_buss):
+        spi_buss.writebytes([0x03])
+
+def request_DIST(spi_buss):
+        spi_buss.writebytes([0x04])
+
+def request_ALL(spi_buss):
+        spi_buss.writebytes([0x05])
+
+def check_ACK(spi_buss):
+        #while True:
+                ack = spi_buss.readbytes(1)[0]
+                print(ack)
+                if ack == 0x11:
+                        return True
+                else:
+                        return False
+
+def read_data(spi_buss, length):
+        i = 0
+        while i < length:
+                print(spi_buss.readbytes(1)[0])
+                i += 1
+    
 def main():
-	#sensmodul_spi.writebytes([0xAA]) #Start SPI
-	struct.pack('f', 3.141592654)
-	print(struct.unpack('f', b'\xdb\x0fI@'))
+        gpio_setup()
+        sensmodul = init_sens_modul()
+        set_mode(sensmodul, 0b00)
+        request_IR(sensmodul)
+       # request_IR(sensmodul)
+        if (check_ACK(sensmodul)):
+                length = spi_buss.readbytes(1)[0]
+                read_data(sensmodul, length)
+        sensmodul.close()
 
 main()
