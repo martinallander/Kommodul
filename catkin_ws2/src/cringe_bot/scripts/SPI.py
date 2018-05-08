1e611aa3c1867e2f1489d390872b9ff443040c8d
TheoW <thewe260@student.liu.se>
2018-05-08 07:50:54 +0000

    Fixed styr-feedback

--- catkin_ws2/src/cringe_bot/scripts/SPI.py
+++ catkin_ws2/src/cringe_bot/scripts/SPI.py
@@ -5,132 +5,142 @@ import binascii
 from Sensor_Data import Sensor_Data
 
 class SPI:
-        def __init__(self, clk_speed):
-                self.sens = spidev.SpiDev()
-                self.sens.open(0, 0)
-                self.sens.max_speed_hz = clk_speed
-                self.styr = spidev.SpiDev()
-                self.styr.open(0, 1)
-                self.styr.max_speed_hz = clk_speed
-                self.sens.mode =  0b00
-                self.styr.mode =  0b00
-                self.length = 0
-                self.done = True
-                self.move_ready = True
-                self.sd = Sensor_Data()
+	def __init__(self, clk_speed):
+		self.sens = spidev.SpiDev()
+		self.sens.open(0, 0)
+		self.sens.max_speed_hz = clk_speed
+		self.styr = spidev.SpiDev()
+		self.styr.open(0, 1)
+		self.styr.max_speed_hz = clk_speed
+		self.sens.mode =  0b00
+		self.styr.mode =  0b00
+		self.length = 0
+		self.done = True
+		self.move_ready = True
+		self.to_publish = False
+		self.last_move = ""
+		self.sd = Sensor_Data()
                 
-        def set_modes(self, module, mode_id):
-        	module.mode = mode_id
+	def set_modes(self, module, mode_id):
+		module.mode = mode_id
 
-        def request_sensor(self, sensor):
-                if sensor.lower() == "all":
-                        self.read("acc")
-                        self.read("angle")
-                        self.read("dist")
-                        self.read("ir")
-                elif sensor.lower() == "acc":
-                        self.length = 12
-                        self.sens.writebytes([0x02])
-                elif sensor.lower() == "angle":
-                        self.length = 12
-                        self.sens.writebytes([0x03])
-                elif sensor.lower() == "dist":
-                        self.length = 4
-                        self.sens.writebytes([0x04])
-                elif sensor.lower() == "ir":
-                        self.length = 256
-                        self.sens.writebytes([0x05])
+	def request_sensor(self, sensor):
+		if sensor.lower() == "all":
+			self.read("acc")
+			self.read("angle")
+			self.read("dist")
+			self.read("ir")
+		elif sensor.lower() == "acc":
+			self.length = 12
+			self.sens.writebytes([0x02])
+		elif sensor.lower() == "angle":
+			self.length = 12
+			self.sens.writebytes([0x03])
+		elif sensor.lower() == "dist":
+			self.length = 4
+			self.sens.writebytes([0x04])
+		elif sensor.lower() == "ir":
+			self.length = 256
+			self.sens.writebytes([0x05])
 
-        def check_ACK(self):
-                ack = self.sens.readbytes(1)[0]
-                #print(ack)
-                if (ack == 0x11):
-                        return True
-                else:
-                        return False
+	def check_ACK(self):
+		ack = self.sens.readbytes(1)[0]
+		if (ack == 0x11):
+			return True
+		else:
+			return False
 
-        def bytes_to_float(self, four_bytes):
-                b_ascii = binascii.hexlify(bytearray(four_bytes))
-                f = struct.unpack('f', b_ascii.decode("hex") )[0]
-                return f
+	def bytes_to_float(self, four_bytes):
+		b_ascii = binascii.hexlify(bytearray(four_bytes))
+		f = struct.unpack('f', b_ascii.decode("hex") )[0]
+		return f
                 
-        def read_sensor(self):
-                sensor_data = list()
-                four_bytes = list()
-                i = 1
-                while i <= self.length:
-                        four_bytes.append(self.sens.readbytes(1)[0])
-                        if i % 4 == 0:
-                                sensor_data.append(self.bytes_to_float(four_bytes))
-                                four_bytes[:] = []
-                        i += 1
-                self.length = 0
-                return sensor_data
+	def read_sensor(self):
+		sensor_data = list()
+		four_bytes = list()
+		i = 1
+		while i <= self.length:
+			four_bytes.append(self.sens.readbytes(1)[0])
+			if i % 4 == 0:
+				sensor_data.append(self.bytes_to_float(four_bytes))
+				four_bytes[:] = []
+			i += 1
+		self.length = 0
+		return sensor_data
 
-        def read(self, sensor):
-                self.done = False
-                if sensor.lower() == "all":
-                        self.request_sensor("all")
-                elif sensor.lower() == "acc":
-                        self.request_sensor("acc")
-                        if self.check_ACK():
-                                self.sd.set_acc(self.read_sensor())
-                                self.done = True
-                        else:
-                                self.done = True
-                elif sensor.lower() == "angle":
-                        self.request_sensor("angle")
-                        if self.check_ACK():
-                                self.sd.set_angle(self.read_sensor())
-                                self.done = True
-                        else:
-                                self.done = True
-                elif sensor.lower() == "dist":
-                        self.request_sensor("dist")
-                        if self.check_ACK():
-                                self.sd.set_dist(self.read_sensor())
-                                self.done = True
-                        else:
-                                self.done = True
-                elif sensor.lower() == "ir":
-                        self.request_sensor("ir")
-                        if self.check_ACK():
-                                self.sd.set_ir(self.read_sensor())
-                                self.done = True
-                        else:
-                                self.done = True
-                else:
-                        self.length = 0
-                        print("unknown request")
+	def read(self, sensor):
+		self.done = False
+		if sensor.lower() == "all":
+			self.request_sensor("all")
+		elif sensor.lower() == "acc":
+			self.request_sensor("acc")
+			if self.check_ACK():
+				self.sd.set_acc(self.read_sensor())
+				self.done = True
+			else:
+				self.done = True
+		elif sensor.lower() == "angle":
+			self.request_sensor("angle")
+			if self.check_ACK():
+				self.sd.set_angle(self.read_sensor())
+				self.done = True
+			else:
+				self.done = True
+		elif sensor.lower() == "dist":
+			self.request_sensor("dist")
+			if self.check_ACK():
+				self.sd.set_dist(self.read_sensor())
+				self.done = True
+			else:
+				self.done = True
+		elif sensor.lower() == "ir":
+			self.request_sensor("ir")
+			if self.check_ACK():
+				self.sd.set_ir(self.read_sensor())
+				self.done = True
+			else:
+				self.done = True
+		else:
+			self.done = True
+			self.length = 0
+			print("unknown request")
 
                 
 
-        def move(self, command):
-                self.done = False
-                if command == "forward":
-			self.styr.writebytes([0x01])
-                elif command == "backward":
-                        self.styr.writebytes([0x02])
-                elif command == "rotleft":
-                        self.styr.writebytes([0x03])
-                elif command == "rotright":
-                        self.styr.writebytes([0x04])
+	def move(self, command):
+		self.done = False
+		if command == "forward":
+			self.styr.writebytes([0x01])	
+		elif command == "backward":
+			self.styr.writebytes([0x02])
+		elif command == "rotleft":
+			self.styr.writebytes([0x03])
+		elif command == "rotright":
+			self.styr.writebytes([0x04])
 		elif command == "turnleft":
-                        self.styr.writebytes([0x05])
+			self.styr.writebytes([0x05])
 		elif command == "turnright":
-                        self.styr.writebytes([0x06])
-                self.check_move_ready()
-                self.done = True
-                return self.move_ready
+			self.styr.writebytes([0x06])
+		if self.check_move_ready():
+			self.last_move = command
+			self.to_publish = True
+		self.done = True
+		return self.move_ready
 
-        def check_move_ready(self):
-                #i = 0
-                #while i < 10:
-                ready = self.styr.readbytes(1)[0]
-                #print("Ready status:")
-                #print(ready)
-                #        i +=1
-                if ready == 0xD0:
-                        self.move_ready = False
-                else:
-                        self.move_ready = True
+	def check_move_ready(self):
+		ready = self.styr.readbytes(1)[0]
+		if ready == 0xFF:
+			self.move_ready = True
+		elif ready == 0xD0:
+			self.move_ready = False
+		else:
+			print("Unknown styr-feedback")
+			self.move_ready = False
+
+	def publish_move(self):
+		if self.to_publish == True:
+			self.to_publish = False
+			return last_move
+		else:
+			return None
+		
--- catkin_ws2/src/cringe_bot/scripts/spi_node.py
+++ catkin_ws2/src/cringe_bot/scripts/spi_node.py
@@ -8,71 +8,64 @@ from std_msgs.msg import String
 from cringe_bot.msg import Sensordata
 
 move_commands = ["rotright", "rotleft", "forward", "backward", "turnleft", "turnright"]
-#global spi = SPI(10000)
 
 def callback(data, spi_node):
     if data.data.lower() in move_commands:
         spi_node.insert_styr_back(data.data.lower())
 	spi_node.perform_action()
-    #else:
-        #spi_node.insert_sens_back(data.data.lower())
     
 
 def listener(spi_node):
-    rospy.init_node('spi_node', anonymous=True)
-    pub = rospy.Publisher('sensor', Sensordata, queue_size=1)
-    rospy.Subscriber('spi_commands', String, callback, spi_node)
-    # spin() simply keeps python from exiting until this node is stopped
-    rate = rospy.Rate(12) # 30hz
-    i = 0
-    rate.sleep()
-    while not rospy.is_shutdown():
-	spi_node.perform_action()
-        sd = Sensordata(spi_node.spi.sd.acc, spi_node.spi.sd.angle, spi_node.spi.sd.ir, spi_node.spi.sd.tof)
-        #rospy.loginfo(sd)
-        pub.publish(sd)
-        rate.sleep()
-    rospy.spin()
-    spi_node.close()
+	rospy.init_node('spi_node', anonymous=True)
+	pub_sensor = rospy.Publisher('sensor', Sensordata, queue_size=1)
+	pub_moves = rospy.Publisher('moves', String, queue_size=1)
+	rospy.Subscriber('spi_commands', String, callback, spi_node)
+	rate = rospy.Rate(12) # 30hz
+	while not rospy.is_shutdown():
+		spi_node.perform_action()
+		last_move = spi_node.spi.publish_move()
+		if last_move != None:
+			pub_moves.publish(last_move)
+		sd = Sensordata(spi_node.spi.sd.acc, spi_node.spi.sd.angle, spi_node.spi.sd.ir, spi_node.spi.sd.tof)
+		pub_sensor.publish(sd)
+		rate.sleep()
+	rospy.spin()
+	spi_node.close()
 
 class SPI_node:
-    def __init__(self):
-        self.spi = SPI(150000)
-        self.styr_queue = list()
-        self.sens_queue = list()
+	def __init__(self):
+		self.spi = SPI(150000)
+		self.styr_queue = list()
+		self.sens_queue = list()
 
-    def insert_sens_back(self, data):
-        self.sens_queue.append(data)
+	def insert_sens_back(self, data):
+		self.sens_queue.append(data)
 
-    def insert_sens_front(self, data):
-        self.sens_queue = [data.data] + spi_node.sens_queue
+	def insert_sens_front(self, data):
+		self.sens_queue = [data.data] + spi_node.sens_queue
 
-    def insert_styr_back(self, data):
-        self.styr_queue.append(data)
+	def insert_styr_back(self, data):
+		self.styr_queue.append(data)
 
-    def insert_styr_front(self, data):
-        self.styr_queue = [data.data] + spi_node.sens_queue
+	def insert_styr_front(self, data):
+		self.styr_queue = [data.data] + spi_node.sens_queue
 
-    def perform_action(self):
-        if(self.spi.done == True):
-            if not len(self.styr_queue) == 0:  
-                if  self.spi.move(self.styr_queue[0]):
-                    self.styr_queue.pop(0)
-                else:
-                     self.spi.read(self.sens_queue.pop(0))
-            else:
-                self.spi.read("acc")
-		self.spi.read("angle")
-		self.spi.read("dist")
-                self.spi.read("ir")
-        else:
-            return None
+	def perform_action(self):
+		if(self.spi.done == True):
+			if not len(self.styr_queue) == 0:  
+				if  self.spi.move(self.styr_queue[0]):
+					self.styr_queue.pop(0)
+			else:
+				self.spi.read("acc")
+				self.spi.read("angle")
+				self.spi.read("dist")
+				self.spi.read("ir")
 
-    def close(self):
-        self.spi.sens.close()
-        self.spi.styr.close()
+	def close(self):
+		self.spi.sens.close()
+		self.spi.styr.close()
 
 if __name__ == '__main__': 
-    print("Starting!")
-    sp = SPI_node()
-    listener(sp)
+	print("Starting!")
+	sp = SPI_node()
+	listener(sp)
