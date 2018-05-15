24b17a62c5f84907c2b4f775283dde4e4da453ef
Martin <martin.allander@gmail.com>
2018-05-15 17:13:41 +0200

    Lidar-fun

--- catkin_ws2/src/rplidar_ros/src/listener.py
+++ catkin_ws2/src/rplidar_ros/src/listener.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python
 # Software License Agreement (BSD License)
 
+import operator
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import LaserScan
 
 def callback(measurement, dist):
-    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', measurement)
-    #dist.publish_closest(measurement.ranges)
-    dist.publish(str(closest(measurement.ranges)))
+    if (dist.done):
+        dist.flip(False)
+        rospy.loginfo(rospy.get_caller_id() + 'I heard %s', measurement)
+        #dist.publish_closest(measurement.ranges)
+        mini = minimum(measurement.ranges)
+        ang = dist.get_angle(measurement.angle_min, measurement.angle_increment, mini[0])
+        dist.publish(str(ang))
+        dist.flip(True)
+    else: 
+        pass
 
-def closest(ranges):
-    return min(ranges)
+
+def closest(values):
+    return min(values)
+
+def minimum(values):
+    min_index, min_value = min(enumerate(values), key=operator.itemgetter(1))
+    return [min_index, min_value]
 
 def listener(Distances):
 
@@ -31,12 +44,19 @@ class Distances():
     def __init__(self):
         self.pub = rospy.Publisher('moves', String, queue_size=1)
         self.closest = 0
+        self.done = True
 
     def publish(self, string):
         self.pub.publish(string)
 
+    def flip(self, val):
+        self.done = val
+
     def closest(self, ranges):
         return min(ranges)
+
+    def get_angle(self, angle_min, angle_inc, index):
+        return angle_min + (angle_inc*index)
 
 if __name__ == '__main__':
     dist = Distances()
